b899bcf3e46e3cd26997cd7f79c0b136ede4f563
JiyoungJeong <stop0@JiyoungJeongui-MacBookPro.local>
2023-08-27 18:23:13 +0900

    Initial commit

---
+++ src/main/java/com/hackstival/hackstival/HackstivalApplication.java
@@ -0,0 +1,13 @@
+package com.hackstival.hackstival;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackstivalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HackstivalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hackstival/hackstival/HackstivalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackstival.hackstival;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackstivalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
